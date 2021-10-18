0056dff8aef2ecfc402f1ed26ad30bb00df55b46
daphnemagsby <daphnemagsby@gmail.com>
2021-10-17 22:24:01 -0500

    Connect to db

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+if(process.env.NODE_ENV !=='production'){
+	require('dotenv').config();
+}
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const indexRouter = require('./routes/index');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -11,5 +15,9 @@ app.use(expressLayouts);
 app.use(express.static('public'));
 
 app.use('/', indexRouter);
+
+mongoose.connect(process.env.DATABASE_URL)
+.then(console.log('Successfully connected to MongoDB'))
+.catch(err=>console.log(err));
 
 app.listen(process.env.PORT || 3000);
